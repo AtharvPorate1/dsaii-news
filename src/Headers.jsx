2d8c2ca781bfc4e05c9c8e941e5a3fc3e39183f1
utsavmehta24 <135753717+utsavmehta24@users.noreply.github.com>
2024-02-22 14:48:05 +0530

    header 1

---
+++ src/Headers.jsx
@@ -0,0 +1,11 @@
+
+const Headers = () => {
+    return (
+        <div className="head">
+            <img src="https://github.com/utsavmehta24/utsavmehta24/blob/main/Dsaii/img/logo.png?raw=true" alt="Logo-Club-Image" />
+            <div className="clubname">DSAII Newsletter</div>
+        </div>
+    );
+}
+
+export default Headers;
