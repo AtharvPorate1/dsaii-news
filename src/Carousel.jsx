dcf2cad1688aaaf78b9f5bb5b501f711973e5584
utsavmehta24 <135753717+utsavmehta24@users.noreply.github.com>
2024-02-27 14:39:47 +0530

    Card Changes

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -7,17 +7,23 @@ class MyCarousel extends Component {
     render() {
         return (
             <Carousel autoPlay={true} interval={3000} infiniteLoop={true}>
-                <div className='image-container-carousel'>
-                    <img src="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=" alt="Slide 1" />
-                    <p className="legend1">Legend 1</p>
+                <div className='carousel-slide'>
+                    <div className='image-container-carousel'>
+                        <img src="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=" alt="Slide 1" />
+                        <p className="legend1">Legend 1</p>
+                    </div>
                 </div>
-                <div className='image-container-carousel'>
-                    <img src="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=" alt="Slide 2" />
-                    <p className="legend1">Legend 2</p>
+                <div className='carousel-slide'>
+                    <div className='image-container-carousel'>
+                        <img src="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=" alt="Slide 2" />
+                        <p className="legend1">Legend 2</p>
+                    </div>
                 </div>
-                <div className='image-container-carousel'>
-                    <img src="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=" alt="Slide 3" />
-                    <p className="legend1">Legend 3</p>
+                <div className='carousel-slide'>
+                    <div className='image-container-carousel'>
+                        <img src="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=" alt="Slide 3" />
+                        <p className="legend1">Legend 3</p>
+                    </div>
                 </div>
             </Carousel>
         );
--- src/cards.jsx
+++ src/cards.jsx
@@ -1,4 +1,31 @@
-const Card = () => {
+import './cards.css';
 
+const Cards = () => {
+  return (
+    <><div className="new">
+      <a href="#" className="container">
+        <img src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*Md9XtZGT0xAelkUsqqu2qw.jpeg" />
+        <h3>Medium Pub Crawl</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro a ea nihil ducimus, enim, perspiciatis consequatur dicta quos fugiat quas adipisci labore veniam. Ut delectus minima ipsa. Accusamus adipisci ipsam placeat, provident maiores numquam, soluta reiciendis velit animi ducimus ut, hic laudantium tempora? Vero minus quia inventore, repellat commodi quod!</p>
+      </a>
+      <a href="#" className="container">
+        <img src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*Md9XtZGT0xAelkUsqqu2qw.jpeg" alt="Responsive image" />
+        <h3>Medium Pub Crawl</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda accusamus iure amet dolores facilis? Eos consequatur earum, sunt, harum autem neque illo cupiditate reiciendis maiores deleniti pariatur nemo itaque voluptatibus quisquam? Quibusdam veniam vero dignissimos! Facere voluptates possimus nihil pariatur officia rerum inventore, exercitationem, illum totam nemo temporibus nulla culpa.</p>
+      </a>
+    </div><div className="new">
+        <a href="#" className="container">
+          <img src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*Md9XtZGT0xAelkUsqqu2qw.jpeg" />
+          <h3>Medium Pub Crawl</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro a ea nihil ducimus, enim, perspiciatis consequatur dicta quos fugiat quas adipisci labore veniam. Ut delectus minima ipsa. Accusamus adipisci ipsam placeat, provident maiores numquam, soluta reiciendis velit animi ducimus ut, hic laudantium tempora? Vero minus quia inventore, repellat commodi quod!</p>
+        </a>
+        <a href="#" className="container">
+          <img src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*Md9XtZGT0xAelkUsqqu2qw.jpeg" alt="Responsive image" />
+          <h3>Medium Pub Crawl</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda accusamus iure amet dolores facilis? Eos consequatur earum, sunt, harum autem neque illo cupiditate reiciendis maiores deleniti pariatur nemo itaque voluptatibus quisquam? Quibusdam veniam vero dignissimos! Facere voluptates possimus nihil pariatur officia rerum inventore, exercitationem, illum totam nemo temporibus nulla culpa.</p>
+        </a>
+      </div></>
+    
+  );
 }
-export default Card;
+export default Cards;
