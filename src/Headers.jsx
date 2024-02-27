c33ae4a66f7387e890ff9a365fea0832778848c9
kashish-234 <kasshhj@gmail.com>
2024-02-27 22:55:49 +0530

    Mobile view updated

--- src/Headers.jsx
+++ src/Headers.jsx
@@ -1,7 +1,14 @@
 const Headers = () => {
     return (
         <div className="head">
-            <img src="Navbar2.png" alt="Logo-Club-Image" />
+            <div className="logo">
+                <img src="club_logo.png" alt="Logo-Club-Image" />
+            </div>
+            <div className="heading">
+                <h1>Data Science &</h1>
+                <h1>Artificial Intelligence</h1>
+                <h1>Inquisitive Club</h1>
+            </div>
         </div>
     );
 }
